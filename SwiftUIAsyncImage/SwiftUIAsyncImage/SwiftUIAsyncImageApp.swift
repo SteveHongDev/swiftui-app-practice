c068d10742d5f08bb3d53f2be14cc2e99abd36ed
Steve Hong <ponoponong@gmail.com>
2022-12-08 16:27:47 +0900

    Dec. 8, 2022
    Start with SwiftUI

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by 홍성범 on 2022/12/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
