56024fbb006c5eefe0b70dbe140be0055393dfc1
Steve Hong <ponoponong@gmail.com>
2022-12-11 14:58:57 +0900

    - Make HomeView
    - Make drag gesture on the footer of the OnboardingView

---
+++ Restart/Restart/Views/CircleGroupView.swift
@@ -0,0 +1,36 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by 홍성범 on 2022/12/11.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //: ZSTACK
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea()
+            
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        }
+    }
+}
