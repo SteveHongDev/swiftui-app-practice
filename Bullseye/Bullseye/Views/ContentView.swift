3d2d0bcb7f19263cbfea1b9f1a0a98a4c9b24ede
Steve Hong <ponoponong@gmail.com>
2022-12-22 14:38:55 +0900

    Bullseye App

--- Bullseye/Bullseye/Views/ContentView.swift
+++ Bullseye/Bullseye/Views/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    @State private var game: Game = Game()
+    
     var body: some View {
         VStack {
             Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
@@ -16,25 +21,32 @@ struct ContentView: View {
                 .lineSpacing(4.0)
                 .font(.footnote)
                 .kerning(2.0)
-            Text("89")
+            Text(String(game.target))
                 .kerning(-1.0)
                 .fontWeight(.black)
                 .font(.largeTitle)
             HStack {
                 Text("1")
                     .font(.headline)
-                Slider(value: .constant(50), in: 1.0...100.0)
+                Slider(value: self.$sliderValue, in: 1.0...100.0)
                 Text("100")
                     .font(.headline)
             }
+            
             Button {
-                
+                print("Hello, SwiftUI!")
+                self.alertIsVisible = true
             } label: {
                 Text("Hit me!")
             }
+            .alert("Hello there!", isPresented: $alertIsVisible) {
+                Button("Awesome!") { }
+            } message: {
+                let roundedValue: Int = Int(self.sliderValue.rounded())
+                Text("The slider's value is \(roundedValue).\n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round.")
+            }
 
         }
-        
     }
 }
 
