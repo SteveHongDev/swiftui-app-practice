ca87497b63a1ddd6b4436d82c8be06cbf544ec03
Steve Hong <ponoponong@gmail.com>
2023-01-06 15:50:18 +0900

    Fructus(Fruits in Spanish) App Start - Set assets

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 홍성범 on 2023/01/06.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
