6c28e738acd575e370f6a9a14a0d5dc680a35fab
Seongbeom Hong <ponoponong@gmail.com>
2023-02-15 16:17:38 +0900

    Add files via upload

---
+++ Hiking/Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by 홍성범 on 2023/02/13.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hiking/Hiking/Models/Hike.swift
@@ -0,0 +1,28 @@
+//
+//  Hike.swift
+//  Hiking
+//
+//  Created by 홍성범 on 2023/02/13.
+//
+
+import Foundation
+
+struct Hike {
+    
+    let name: String
+    let imageURL: String
+    let miles: Double
+    
+}
+
+extension Hike {
+    
+    static func all() -> [Hike] {
+        
+        return [
+            Hike(name: "Salmonberry Trail", imageURL: "sal", miles: 6),
+            Hike(name: "Tom, Dick, and Harry Mountain", imageURL: "tom", miles: 5.8),
+            Hike(name: "Tamanawas Falls", imageURL: "tam", miles: 5)
+        ]
+    }
+}
