465c1639fc78cc09a7e5d40db4d9d61b5b57fa5a
Seongbeom Hong <ponoponong@gmail.com>
2023-02-23 21:04:14 +0900

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Steve Hong on 2023/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
