6ea70061bff1b01762cc4bd9c849d7c7e88486fc
Seongbeom Hong <sb12@tuta.io>
2023-02-26 11:07:54 +0900

    Chapter 4 (DOING)

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Steve Hong on 2023/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Steve Hong on 2023/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
+    var body: some View {
+        VStack {
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+                .labelsHidden() // 시각 장애인을 위해 label을 남겨둔다.(VoiceOver)
+            
+        }
+        .padding()
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
