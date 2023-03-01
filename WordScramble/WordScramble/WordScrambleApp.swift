b570af1553261197544d3fab1b574c02b81a3dee
Seongbeom Hong <sb12@tuta.io>
2023-03-01 10:28:00 +0900

    Chapter 5 (START)

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Steve Hong on 2023/03/01.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
