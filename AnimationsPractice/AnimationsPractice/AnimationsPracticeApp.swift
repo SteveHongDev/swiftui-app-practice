3e89291b509f504d34017873ae4451ab9acf973d
Seongbeom Hong <sb12@tuta.io>
2023-03-03 15:29:27 +0900

    Chapter 6 (Doing)

---
+++ AnimationsPractice/AnimationsPractice/AnimationsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsPracticeApp.swift
+//  AnimationsPractice
+//
+//  Created by Steve Hong on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
