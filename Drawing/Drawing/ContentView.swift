b6d44b36839bc1f035fb33aa64ce520d685980a9
Seongbeom Hong <sb12@tuta.io>
2023-03-19 12:35:21 +0900

    Chapter 9 (Done)

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,44 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(.blue, lineWidth: 40)
-    }
-}
-
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.move(to: CGPoint(x: 200, y: 100))
-        path.addLine(to: CGPoint(x: 100, y: 300))
-        path.addLine(to: CGPoint(x: 300, y: 300))
-        path.addLine(to: CGPoint(x: 200, y: 100))
-        
-        return path
-    }
-}
-
-struct Arc: InsettableShape {
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
-    var insetAmount = 0.0
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: startAngle, endAngle: endAngle, clockwise: !clockwise)
-        
-        return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        
-        return arc
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("singapore"), scale: 0.1), lineWidth: 20)
+            .frame(width: 300, height: 200)
     }
 }
 
